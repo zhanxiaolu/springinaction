cbe1614e81cb900d19e25a1ac4179028bc9e0b4c
zhanxiaolu <290581396@qq.com>
2016-09-08 20:23:57 +0800

    new

---
+++ knights/src/main/java/com/springinaction/knights/Minstrel.java
@@ -0,0 +1,16 @@
+package com.springinaction.knights;
+/**
+ * ¸èÕß£¬AOPÍ¨Öª
+ * @author Administrator
+ *
+ */
+public class Minstrel {
+  public void singBeforeQuest() {     //<co id="co_singBefore"/>
+    System.out.println("Fa la la; The knight is so brave!");
+  }
+  
+  public void singAfterQuest() {     //<co id="co_singAfter"/>
+    System.out.println(
+            "Tee hee he; The brave knight did embark on a quest!");
+  }
+}
